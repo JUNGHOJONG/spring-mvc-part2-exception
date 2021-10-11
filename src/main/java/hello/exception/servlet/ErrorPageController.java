ebfde919b09a6edb461566db9c8d8a50c8a44a5f
JUNGHOJONG <hojong1351@naver.com>
2021-10-11 16:23:49 +0900

    에러 페이지 GET, POST 모두 처리 하도록 변경

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -3,18 +3,21 @@ package hello.exception.servlet;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
 @Controller
 public class ErrorPageController {
 
-    @GetMapping("/error-page/404")
+    @RequestMapping("/error-page/404") // GET, POST 모두 처리
+//    @GetMapping("/error-page/404")
     public String errorPage404() {
         log.info("error page 404");
         return "/error-page/404";
     }
 
-    @GetMapping("/error-page/500")
+    @RequestMapping("/error-page/500")
+//    @GetMapping("/error-page/500")
     public String errorPage500() {
         log.info("error page 500");
         return "/error-page/500";
